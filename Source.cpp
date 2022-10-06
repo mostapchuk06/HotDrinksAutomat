c86c7f238b9baaa9e9928bb63518dce292707c71
mostapchuk06 <101934840+mostapchuk06@users.noreply.github.com>
2022-10-06 07:21:55 +0000

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -11,64 +11,97 @@ struct Drink
 	DrinkSize drink_size;
 	int drink_price;
 
-	Drink(string n, DrinkSize s, int p) : name(n), drink_size(s), drink_price(p)
+	Drink(string nam, DrinkSize s, int p) : name(nam), drink_size(s), drink_price(p)
 	{ }
 
 };
 
+struct Ingridiants
+{
+	string name;
+	int number;
+
+	Ingridiants(string nam, int num) : name(nam), number(num)
+	{ }
+};
+
 class Machine
 {
+private:
 	vector<Drink> drink_list;
-	int cash;
 
-	string l;
-	string pass;
+	vector<Ingridiants> ingridiants_list;
+	string i;
+	int n;
+	int cl, cxl, cks;
+	int tl, txl, tks;
+	int cuppl, cuppxl, cuppks;
+
+	string standart_l;
+	string standart_pass;
+	
+	int cash;
 public:
 
 	void LogAndPass()
 	{
-		this->l = "L";
-		this->pass = "0";
-		cout << "To enter the admin mode, enter your login and password : ";
-		cin >> l;
-		cin >> pass;
-		if (l == "L" && pass == "0")
+		int audit;
+		cout << "Would you like to sign in admin mode(0==NO, 1==Yes) : ";
+		cin >> audit;
+		cout << endl;
+		if (audit == 1)
 		{
-			cout << "Authorization is successful" << endl;
+			standart_l = "L";
+			standart_pass = "0";
+			cout << "To enter the admin mode, enter your login and password : ";
+			cin >> standart_l;
+			cin >> standart_pass;
+			if (standart_l == "L" && standart_pass == "0")
+			{
+				cout << "Authorization is successful" << endl;
+			}
+		}
+		else
+		{
+			cout << "...Incorrect.data..." << endl;
 		}
 	}
 
 	Machine()
 	{
-		drink_list.push_back(Drink("Coffee", DrinkSize::Large, 20));
-		drink_list.push_back(Drink("Coffee", DrinkSize::Xlarge, 25));
-		drink_list.push_back(Drink("Coffee", DrinkSize::KingSize, 30));
+		cout << "Enter price for you drinks : ";
+		cin >> cl;
+		cin >> cxl;
+		cin >> cks;
+		cin >> tl;
+		cin >> txl;
+		cin >> tks;
+		cin >> cuppl;
+		cin >> cuppxl;
+		cin >> cuppks;
+		drink_list.push_back(Drink("Coffee", DrinkSize::Large, cl));
+		drink_list.push_back(Drink("Coffee", DrinkSize::Xlarge, cxl));
+		drink_list.push_back(Drink("Coffee", DrinkSize::KingSize, cks));
 
-		drink_list.push_back(Drink("Tea", DrinkSize::Large, 18));
-		drink_list.push_back(Drink("Tea", DrinkSize::Xlarge, 22));
-		drink_list.push_back(Drink("Tea", DrinkSize::KingSize, 28));
+		drink_list.push_back(Drink("Tea", DrinkSize::Large, tl));
+		drink_list.push_back(Drink("Tea", DrinkSize::Xlarge, txl));
+		drink_list.push_back(Drink("Tea", DrinkSize::KingSize, tks));
 
-		drink_list.push_back(Drink("Cuppucino", DrinkSize::Large, 19));
-		drink_list.push_back(Drink("Cuppucino", DrinkSize::Xlarge, 23));
-		drink_list.push_back(Drink("Cuppucino", DrinkSize::KingSize, 29));
+		drink_list.push_back(Drink("Cuppucino", DrinkSize::Large, cuppl));
+		drink_list.push_back(Drink("Cuppucino", DrinkSize::Xlarge, cuppxl));
+		drink_list.push_back(Drink("Cuppucino", DrinkSize::KingSize, cuppks));
 	}
 
 	void AdminIngridiants()
 	{
-		string ingridiants;
-		int audit;
-		cout << "Would you like to add ne ingridiants?(1==Yes, 0==No) : ";
-		cin >> audit;
-		if (audit == 1)
-		{
-			cout << "Enter your ingridiants : \n";
-			cout << "Tip :: Milk,Sugar,Water, etc.\n";
-			cin >> ingridiants;
-			cout << "Your ingridiants : " << ingridiants;
-		}
+		cout << "Add your ingridiants and ingridiants number : " << endl;
+		cout << "Tip :: Milk,Sugar,Water, etc.\n";
+		cin >> i;
+		cin >> n;
+		ingridiants_list.push_back(Ingridiants(i, n));
 		cout << endl;
 	}
-	
+
 	void Order(string name, DrinkSize size)
 	{
 		for (auto drink : drink_list)
@@ -99,7 +132,17 @@ public:
 			cash = 0;
 			cout << "Cash : " << cash << endl;
 		}
-		
+
+	}
+
+	void IngShow() const
+	{
+		cout << "============================ INGRIDIANTS ================================\n";
+		for (auto ingridiants : ingridiants_list)
+		{
+			cout << ingridiants.name << " " << ingridiants.number << " g ";
+		}
+		cout << "=========================================================================\n\n";
 	}
 
 	void Show() const
@@ -124,11 +167,19 @@ public:
 
 int main()
 {
+	bool a = 1;
 	Machine m1;
 	m1.Show();
 	m1.Order("Coffee", DrinkSize::Large);
-	//m1.LogAndPass();
-	m1.AdminIngridiants();
+	m1.LogAndPass();
+	do
+	{
+		m1.AdminIngridiants();
+		cout << "Have you downloaded all the required ingredients?\n";
+		cout << "Enter 0 if so or 1 if not so : ";
+		cin >> a;
+	} while (!a == 0);
+	m1.IngShow();
 	m1.Cash();
-	
+
 }
